c47ff451f70d64734d39b7d081d952fa102e0ccd
zerajudeen-zera <zerajudeen@gmail.com>
2025-09-02 22:24:00 +0530

    updated a new line at last end

--- info.sh
+++ info.sh
@@ -5,3 +5,4 @@ echo "This is my Host name $(hostname) and the date is $(date)"
 echo 'Added new line'
 echo 'Added new line at last'
 echo 'Added new line at last end'
+echo 'Added a new line at last end'
